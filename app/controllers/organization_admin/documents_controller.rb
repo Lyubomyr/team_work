ab5b44adfd7e1729f5543f15f57b09a8a4e66709
bokalo <gist03@ukr.net>
2017-07-10 22:39:31 +0300

    Try to add download file

--- app/controllers/organization_admin/documents_controller.rb
+++ app/controllers/organization_admin/documents_controller.rb
@@ -4,6 +4,20 @@ class OrganizationAdmin::DocumentsController < OrganizationAdmin::AdminControlle
     @file_group = FileGroup.find(params[:file_group_id])
     @document = @file_group.documents.create(document_params)
     redirect_to organization_admin_file_group_path(@file_group)
+
+  # uploaded_io = params[:document][:file]
+  #   File.open(Rails.root.join('public', 'assets', 'products', uploaded_io.original_filename), 'wb') do |f|
+  #     f.write(uploaded_io.read)
+  #   end
+  # @document.file = uploaded_io.original_filename
+
+  end
+
+  def show
+    @file_group = FileGroup.find(params[:file_group_id])
+    @document = Document.find(params[:id])
+    extension = @document.file.to_s.split('.')
+      send_file Rails.root.join('public', 'assets', 'products', @document.file), :type => "application/#{extension[1]}", :x_sendfile=>true
   end
 
   def destroy
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,8 @@
 class Document < ActiveRecord::Base
   belongs_to :file_group
 
-  has_attached_file :file
+  has_attached_file :file,
+                    :url => ":basename.:extension",
+                    :path => ":rails_root/public/assets/products/:basename.:extension"
   validates_attachment :file, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 end
