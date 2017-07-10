d21e581cfb1a4d1c04e711ef942c020706790603
bokalo <gist03@ukr.net>
2017-07-10 15:06:12 +0300

    Add ability to delete documents

--- app/controllers/organization_admin/documents_controller.rb
+++ app/controllers/organization_admin/documents_controller.rb
@@ -6,6 +6,14 @@ class OrganizationAdmin::DocumentsController < OrganizationAdmin::AdminControlle
     redirect_to organization_admin_file_group_path(@file_group)
   end
 
+  def destroy
+    @file_group = FileGroup.find(params[:file_group_id])
+    @document = Document.find(params[:id])
+    @document.destroy
+    redirect_to organization_admin_file_group_path(@file_group), notice: 'Document was successfully deleted.'
+  end
+
+
   private
     def document_params
       params.require(:document).permit(:name, :file, :position)
