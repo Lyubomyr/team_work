6af1e7d5889b1f820ca261a49e5aaf88e5dc1b58
bokalo <gist03@ukr.net>
2017-07-11 10:21:09 +0300

    Add ability to organization admin upload/download files

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ActiveRecord::Base
   belongs_to :file_group
 
   has_attached_file :file,
-                    :url => ":basename.:extension",
+                    :url => "/assets/products/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:basename.:extension"
   validates_attachment :file, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 end
